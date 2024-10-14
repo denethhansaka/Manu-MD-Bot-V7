be0bee6e78041ad7cfdaf7d9ac6b38981fc8919a
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-15 00:19:24 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "gmlEQQgK#I3p8_RkDchmZYAjNsuKWATOwQIJjyJW6C_ReK7-4oMY",
-ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
-ALIVE_MSG : process.env.ALIVE_MSG || "Put your alive messege",
-MENU_IMG : process.env.MENU_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
+SESSION_ID: process.env.SESSION_ID || "",
+ALIVE_IMG : process.env.ALIVE_IMG || "https://github.com/denethhansaka/DENETH-MD-New/blob/main/Images/DENETH-MD.jpg?raw=true",
+ALIVE_MSG : process.env.ALIVE_MSG || "Hey,I Am DENETH-MD.I am Alive Now",
+MENU_IMG : process.env.MENU_IMG || "https://github.com/denethhansaka/DENETH-MD-New/blob/main/Images/DENETH-MD.jpg?raw=true",
 MODE : process.env.MODE || "public",    
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_BIO: process.env.AUTO_BIO || "true",
